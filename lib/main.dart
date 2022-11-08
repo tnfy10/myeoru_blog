60307f9b8222e55a04424774b43f0360ddf91ee6
Hong Sung-hwi <tnfy10@gmail.com>
2022-11-08 21:33:54 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         useMaterial3: true
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: ''),
     );
   }
 }
